ebe412e17023485fad0890a15d6b9aadde70a6dd
Taylor Fromm <taylorsmithfromm@gmail.com>
2019-05-01 14:02:37 -0400

    Taylors update. everything working thus far

---
+++ currencyapp/src/Components/Login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Login extends React.Component {
+
+}; //end class Login
+
+export default Login
--- currencyapp/src/Components/Navigation.js
+++ currencyapp/src/Components/Navigation.js
@@ -9,7 +9,12 @@ const Navigation = () => {
             <img src={leftArrow} alt="arrow" class="arrowImage"></img>
             Back to Home 
             </NavLink>
-            <NavLink to="/ConversionComp">Conversions</NavLink>
+            <NavLink to="/China">China</NavLink>
+            <NavLink to="/India">India</NavLink>
+            <NavLink to="/UK">UK</NavLink>
+            <NavLink to="/US">US</NavLink>
+            <NavLink to="/EU">EU</NavLink>
+            <NavLink to="/Login">Login</NavLink>
             
         </div>
     )
