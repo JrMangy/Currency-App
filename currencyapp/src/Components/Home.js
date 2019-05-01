db9673122254276598c21fc9d0fb0f4b254e7ec6
Ronnie Stevenson <jrstevenson09@gmail.com>
2019-05-01 16:42:47 -0400

    radio buttons

--- currencyapp/src/Components/Home.js
+++ currencyapp/src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import firebase from 'firebase'
 import '../CSS/Home.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 var config = {
@@ -21,34 +22,10 @@ class Home extends React.Component {
         super();
         this.state = {
             text: "I'm the G.O.A.T.",
-            displayMenu: false
+            size: ''
         }
-        this.showDropdownMenu1 = this.showDropdownMenu1.bind(this);
-        this.showDropdownMenu2 = this.showDropdownMenu2.bind(this);
-        this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
     }
 
-
-
-    showDropdownMenu1(event) {
-        event.preventDefault();
-        this.setState({ displayMenu: true }, () => {
-            document.addEventListener('click', this.hideDropdownMenu);
-        });
-    }
-    showDropdownMenu2(event) {
-        event.preventDefault();
-        this.setState({ displayMenu: true }, () => {
-            document.addEventListener('click', this.hideDropdownMenu);
-        });
-    }
-
-    hideDropdownMenu() {
-        this.setState({ displayMenu: false }, () => {
-            document.removeEventListener('click', this.hideDropdownMenu);
-        });
-
-    }
     componentDidMount() {
         const textRef = database.ref("text/");
         textRef.on("value", snapshot => {
@@ -67,48 +44,44 @@ class Home extends React.Component {
     render() {
         return (
             <div className="Background">
-                <div className="Header">
-                    <div className="dropdown" style={{ background: "red", width: "200px" }} >
-                        <div className="button" onClick={this.showDropdownMenu1}>Pick a starting currency: </div>
+                <div className="App-header">
 
-                        {this.state.displayMenu ? (
-                            <ul>
-                                <li><a href="#Manage Pages">USD $</a></li>
-                                <li><a href="#Create Ads">Euro &#x20AC;</a></li>
-                                <li><a href="#Manage Ads">Pound &#xa3;</a></li>
-                                <li><a href="#Activity Logs">Rupee &#x20A8;</a></li>
-                                <li><a href="#Setting">Yuan &#x5143;</a></li>
-                            </ul>
-                        ) :
-                            (
-                                null
-                            )
-                        }
-                    </div>
-                    <div classname="textBox">
-                    
-                    </div>
-                    <div className="dropdown" >
-                        <div className="button" onClick={this.showDropdownMenu2}>Pick currency to convert to: </div>
-
-                        {this.state.displayMenu ? (
-                            <ul>
-                                <li><a href="#Manage Pages">USD $</a></li>
-                                <li><a href="#Create Ads">Euro &#x20AC;</a></li>
-                                <li><a href="#Manage Ads">Pound &#xa3;</a></li>
-                                <li><a href="#Activity Logs">Rupee &#x20A8;</a></li>
-                                <li><a href="#Setting">Yuan &#x5143;</a></li>
-                            </ul>
-                        ) :
-                            (
-                                null
-                            )
-                        }
-                        
-                    </div>
+                    <form>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" checked />USD $
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" />Euro &#2080;
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" />Pound &#x
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" />Option 2
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" />Option 3
+                        </label>
+                    </form>
+                    <form>
+                        <label for="exampleInputEmail1">Enter your amount: </label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    <form>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" checked />Option 1
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" />Option 2
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="optradio" />Option 3
+                        </label>
+                    </form>
                 </div>
             </div>
-        )
+        );
     }
 }
 
