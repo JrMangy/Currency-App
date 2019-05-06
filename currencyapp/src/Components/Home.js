69f4709b86339825b3944c9cace28772d569c921
Ronnie Stevenson <jrstevenson09@gmail.com>
2019-05-06 10:04:25 -0400

    css changes

--- currencyapp/src/Components/Home.js
+++ currencyapp/src/Components/Home.js
@@ -16,17 +16,15 @@ var config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 // console.log(firebase);
-var ref = database.ref('currency');
-var valueUS = {
-    name: 'USD',
-    value: 834
-}
-ref.push(valueUS);
+// var ref = database.ref('currency');
+// var valueUS = {
+//     name: 'USD',
+//     value: 834
+// }  
+// var inputCurrency = firebase.database().ref('')
 
 
 class Home extends React.Component {
-
-
     constructor() {
         super();
         this.state = {
@@ -34,60 +32,55 @@ class Home extends React.Component {
             size: ''
         }
     }
+    
 
-    componentDidMount() {
-        const textRef = database.ref("text/");
-        textRef.on("value", snapshot => {
-            this.setState({
-                text: snapshot.val()
-            })
-        });
-    }
-    writeData = e => {
-        e.preventDefault();
-        const textValue = e.target.elements.inputText.value;
-        database.ref("text/").set(textValue, function (error) {
-            error ? alert("error") : console.log("it works")
-        })
-    }
     render() {
         return (
-            <div className="Background">
+            <div className="background">
                 <div className="App-header">
-
+                    <p className="oblique">Which currency would you like to switch from?</p>
                     <form>
                         <label >
-                            <input className="radio-inline1" type="radio" value = "USD" name="optradio" checked/>USD $
-                        </label>
-                        <label className="radio-inline2">
-                            <input type="radio" value = "Euro" name="optradio"/>Euro &euro;   
-                        </label>
-                        <label className="radio-inline3">
-                            <input type="radio" value = "Pound" name="optradio"/>Pound &#xa3;
-                        </label>
-                        <label className="radio-inline4">
-                            <input type="radio" value = "Rupee" name="optradio" />Rupee &#x20A8;
-                        </label>
-                        <label className="radio-inline5">
-                            <input type="radio" value = "Yuan"name="optradio" />Yuan &#x5143;
+                            <input className="form-radio" type="radio" value="USD" name="optradio" />
+                            <label htmlFor="USD">USD $</label>
+                            <input className="form-radio" type="radio" value="Euro" name="optradio" />
+                            <label htmlFor="USD">Euro &euro;</label>
+                            <input className="form-radio" type="radio" value="Pound" name="optradio" />
+                            <label htmlFor="USD">Pound &#xa3;</label>
+                            <input className="form-radio" type="radio" value="Rupee" name="optradio" />
+                            <label htmlFor="USD">Rupee &#x20A8;</label>
+                            <input className="form-radio" type="radio" value="Yuan" name="optradio" />
+                            <label htmlFor="USD">Yuan &#x5143;</label>
                         </label>
                     </form>
+                    <br />
+                    <br />
                     <form>
-                        <label for="exampleInputEmail1">Enter currency amount: </label>
-                        <input type="number" className="currencyInput" placeholder="Enter amount" />
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <label htmlFor="currencyInput">Enter currency amount: </label>
+                        <input type="number" className="currencyInput" placeholder="Enter amount" id="inputBox" />
+                        <button type="submit" className="submitButton">Submit</button>
                     </form>
+                    <br />
+                    <br />
                     <form>
-                        <label className="radio-inline6">
-                            <input type="radio" name="optradio2" checked />Option 1
-                        </label>
-                        <label className="radio-inline6">
-                            <input type="radio" name="optradio2" />Option 2
-                        </label>
-                        <label className="radio-inline6">
-                            <input type="radio" name="optradio2" />Option 3
+                        <p>Which currency would you like to switch to?</p>
+                        <label >
+                            <input className="form-radio" type="radio" value="USD1" name="optradio" />
+                            <label htmlFor="USD">USD $</label>
+                            <input className="form-radio" type="radio" value="Euro1" name="optradio" />
+                            <label htmlFor="USD">Euro &euro;</label>
+                            <input className="form-radio" type="radio" value="Pound1" name="optradio" />
+                            <label htmlFor="USD">Pound &#xa3;</label>
+                            <input className="form-radio" type="radio" value="Rupee1" name="optradio" />
+                            <label htmlFor="USD">Rupee &#x20A8;</label>
+                            <input className="form-radio" type="radio" value="Yuan1" name="optradio" />
+                            <label htmlFor="USD">Yuan &#x5143;</label>
                         </label>
                     </form>
+                    <pre id='object'></pre>
+                    <ul id='list'>
+                    
+                    </ul>
                 </div>
             </div>
         );
