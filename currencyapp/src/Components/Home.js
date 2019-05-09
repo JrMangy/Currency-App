2456fd5717ea26a22beaffb5f2ec2d2159e12b35
Ronnie Stevenson <jrstevenson09@gmail.com>
2019-05-09 14:38:41 -0400

    Newest changes

--- currencyapp/src/Components/Home.js
+++ currencyapp/src/Components/Home.js
@@ -3,37 +3,10 @@ import '../CSS/Home.css'
 import InputBox from './Home Components/inputBox.js'
 import RadioOne from './Home Components/radioOne.js'
 import RadioTwo from './Home Components/radioTwo.js'
-import database from './firebase'
 
 class Home extends React.Component {
     constructor() {
         super();
-        this.state = {
-            userValue: 0,
-            toDollar: 0,
-            fromDollar: 0,
-            output: 0,
-        }
-    }
-
-    componentDidMount(){
-        const userValue = database.ref('UserInput/value/');
-        userValue.on('value', snap => {
-            this.setState({ userValue: snap.val() });
-        });
- 
-        const toDollar = database.ref('ToDollarValue/value/');
-        toDollar.on('value', snap => {
-            this.setState({ toDollar: snap.val() });
-        });
-        const fromDollar = database.ref('FromDollarValue/value/');
-        fromDollar.on('value', snap => {
-            this.setState({ fromDollar: snap.val() });
-        });
-        const output = database.ref('OutputValue/value/');
-        output.on('value', snap => {
-            this.setState({output: snap.val()})
-        })
     }
     render() {
         return (
@@ -42,7 +15,6 @@ class Home extends React.Component {
                     <RadioOne />
                     <br />
                     <br />
-                    <p>Which currency would you like to switch to?</p>
                     <RadioTwo />
                     <br />
                     <br />
