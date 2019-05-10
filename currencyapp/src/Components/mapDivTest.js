006877a6d5c028b051ecb818553589ed88ef2989
Taylor Fromm <taylorsmithfromm@gmail.com>
2019-05-10 09:28:37 -0400

    Taylor in process of map

--- currencyapp/src/Components/mapDivTest.js
+++ currencyapp/src/Components/mapDivTest.js
@@ -1,26 +1,32 @@
-// constructor {
-//     super();
-//     state = {
-//         map: this.map
-//     }
-//     }
-    
-//     function initMap() {
-//       // The location of destination
-//       var uluru = {lat: -25.344, lng: 131.036};
-//       // The map, centered at marker
-//       var map = new google.maps.Map(
-//           this.map, {zoom: 4, center: uluru});
-//       // The marker, positioned at Uluru
-//       var marker = new google.maps.Marker({position: uluru, map: map});
-//     }
+import React from 'react';
+import firebase from 'firebase';
 
-//     {*Add to render*}
-// <div id="map"></div>
+
+class Map extends React.Component {
+constructor() {
+    super();
+    // state = {
+    //     map: this.map
+    // }
+    }
+    
+  initMap() {
+      // The location of destination
+      var uluru = {lat: -25.344, lng: 131.036};
+      // The map, centered at marker
+      var map = new google.maps.Map(
+          this.map, {zoom: 4, center: uluru});
+      // The marker, positioned at Uluru
+      var marker = new google.maps.Marker({position: uluru, map: map});
+    } 
+    render() {
+        return(
+        <div id="map"></div>)}
+}
+export default Map
+//   
 // Replace the value of the key parameter with your own API key.
-// <script async defer
-// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap">
-// </script>
+
 
 // #map {
 //   height: 400px;
